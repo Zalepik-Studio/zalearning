b31861d5c597a17b5e5215e7aaf368d93994f246
amarNrddn <amarnrddn02@gmail.com>
2024-03-06 20:18:36 +0700

    Refactor:  Break down sedibar  into layouts

---
+++ quiz_questions.sql
@@ -0,0 +1,11 @@
+INSERT INTO quiz_questions (class_id, question, option_a, option_b, option_c, option_d, correct_answer, created_at, updated_at) 
+VALUES 
+('1', 'Apa itu DOM (Document Object Model) dalam JavaScript?', 'a. Model untuk memanipulasi HTML dan XML', 'b. Bahasa pemrograman JavaScript', 'c. Sistem manajemen basis data', 'd. Protokol komunikasi internet', 'a', NOW(), NOW()),
+('1', 'Bagaimana cara membuat fungsi di JavaScript?', 'a. create function myFunction()', 'b. function = myFunction()', 'c. def myFunction():', 'd. function myFunction()', 'd', NOW(), NOW()),
+('1', 'Apa yang dimaksud dengan "closure" dalam konteks JavaScript?', 'a. Sebuah fungsi yang dapat diakses dari mana saja', 'b. Sebuah fungsi bersarang yang memiliki akses ke variabel-variabel di lingkungannya', 'c. Sebuah metode untuk menutup aplikasi', 'd. Sebuah fungsi yang tidak dapat diubah setelah dideklarasikan', 'b', NOW(), NOW()),
+('1', 'Apa itu AJAX dalam JavaScript?', 'a. Sebuah teknik untuk mengirim dan menerima data secara asynchronous', 'b. Sebuah library untuk membuat animasi', 'c. Sebuah metode untuk memvalidasi formulir', 'd. Sebuah fungsi untuk menggambar grafik', 'a', NOW(), NOW()),
+('1', 'Bagaimana cara menghandle error di JavaScript?', 'a. Menggunakan try-catch statement', 'b. Menggunakan alert box', 'c. Menggunakan console.log()', 'd. Semua jawaban benar', 'a', NOW(), NOW()),
+('1', 'Apa itu JSON (JavaScript Object Notation)?', 'a. Sebuah bahasa pemrograman', 'b. Sebuah format pertukaran data', 'c. Sebuah metode pengambilan data dari database', 'd. Sebuah fungsi matematis', 'b', NOW(), NOW()),
+('1', 'Bagaimana cara membuat loop (perulangan) di JavaScript?', 'a. for loop', 'b. while loop', 'c. do-while loop', 'd. Semua jawaban benar', 'd', NOW(), NOW()),
+('1', 'Apa kegunaan dari "strict mode" dalam JavaScript?', 'a. Memaksa variabel untuk dideklarasikan sebelum digunakan', 'b. Mempercepat eksekusi kode', 'c. Memungkinkan penggunaan variabel tanpa deklarasi', 'd. Tidak ada perbedaan', 'a', NOW(), NOW()),
+('1', 'Apa itu event bubbling dalam JavaScript?', 'a. Sebuah metode animasi', 'b. Sebuah teknik penyusunan elemen HTML', 'c. Sebuah properti objek', 'd. Sebuah peristiwa dimulai dari elemen terdalam dan naik ke elemen terluar', 'd', NOW(), NOW());
