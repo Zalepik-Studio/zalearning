c61a851bcfa676426eba04990972fde27c8f4e2d
heriwahyudiono <99733430+heriwahyudiono@users.noreply.github.com>
2024-03-08 06:40:44 +0700

    Update zalearning.sql

--- zalearning.sql
+++ zalearning.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE zalearning;
 
-USE zalearning; 
+USE zalearning;
 
 CREATE TABLE users (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
     gender VARCHAR(255),
     date_of_birth DATE,
@@ -13,31 +13,30 @@ CREATE TABLE users (
     role ENUM('admin', 'user'),
     token VARCHAR(255) NULL,
     token_expired_at TIMESTAMP NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE classes (
-
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     class_name VARCHAR(255),
     class_thumbnail VARCHAR(255),
     class_desc TEXT,
     class_route VARCHAR(255),
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE user_classes (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT(11),
     class_id INT(11),
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE quiz_questions (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     class_id INT,
     question TEXT,
     option_a VARCHAR(255),
@@ -45,38 +44,34 @@ CREATE TABLE quiz_questions (
     option_c VARCHAR(255),
     option_d VARCHAR(255),
     correct_answer CHAR(1),
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (class_id) REFERENCES classes(id)
 );
 
 CREATE TABLE quiz_answers (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
     class_id INT,
     user_quiz_id VARCHAR(255),
     question_id INT,
     user_answer CHAR(1),
     true_or_false BOOLEAN,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (class_id) REFERENCES classes(id),
     FOREIGN KEY (question_id) REFERENCES quiz_questions(id)
 );
 
 CREATE TABLE quiz_scores (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
     class_id INT,
     user_quiz_id VARCHAR(255),
     score INT,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (class_id) REFERENCES classes(id)
 );
-
-
-
-
