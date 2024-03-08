68a30c4d437b9de56ec4def07e6c2889ed05edbd
heriwahyudiono <99733430+heriwahyudiono@users.noreply.github.com>
2024-03-08 07:03:20 +0700

    feat: update UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,13 +15,13 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'name' => 'John Doe',
-            'gender' => 'male',
-            'date_of_birth' => '1990-01-01',
-            'email' => 'john@example.com',
-            'phone_number' => '123456789',
-            'password' => Hash::make('rahasia'),
-            'role' => 'user',
+            'name' => 'Admin',
+            'gender' => null,
+            'date_of_birth' => '0000-00-00',
+            'email' => 'admin@zalepik.com',
+            'phone_number' => '085720075826',
+            'password' => Hash::make('123456'),
+            'role' => 'admin',
             'token' => null,
             'token_expired_at' => null,
             'created_at' => now(),
